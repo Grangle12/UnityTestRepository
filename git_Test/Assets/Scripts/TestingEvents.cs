54c3116b78e8b473fdb14c4d2ed8845467620a80
Grangle12 <grangle@gmail.com>
2025-01-18 15:36:12 -0800

    Updated Code

---
+++ git_Test/Assets/Scripts/TestingEvents.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingEvents : MonoBehaviour
+{
+    public event EventHandler<OnSpacePressedEventArges> OnSpacePressed;
+    public class OnSpacePressedEventArges : EventArgs
+    {
+        public int spaceCount;
+    }
+
+    public event TestEventDelegate OnFloatEvent;
+    public delegate void TestEventDelegate(float f);
+
+    public event Action<bool, int> OnActionEvent;
+
+
+
+    private int spaceCount;
+
+    private void Start()
+    {
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Space Presed
+            /*if (OnSpacePressed != null)
+            {
+                OnSpacePressed(this, EventArgs.Empty);
+            }
+            else
+            {
+                Debug.Log("no subscribers to OnSpacePressed");
+            }*/
+            spaceCount++;
+            OnSpacePressed?.Invoke(this, new OnSpacePressedEventArges { spaceCount = spaceCount } );
+
+            OnFloatEvent?.Invoke(5.5f);
+
+            OnActionEvent?.Invoke(true, 56);
+        }
+    }
+}
